49b610b9b86a6d8387ab808c0a6131756f210824
crawlingweapon <crawling.weapon700@ok9.io>
2026-06-12 13:58:06 -0500

    Add packages/apache2-pkg/index.js

---
+++ packages/apache2-pkg/index.js
@@ -0,0 +1,4 @@
+// @license-testbed/apache2-pkg
+// License: Apache-2.0
+// Demo package under Apache-2.0 — permissive with patent grant
+module.exports = { name: '@license-testbed/apache2-pkg', license: 'Apache-2.0' };
