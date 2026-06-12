b7db32de9013777ea0ec36f9ef49fa209c3c9377
crawlingweapon <crawling.weapon700@ok9.io>
2026-06-12 13:58:08 -0500

    Add packages/bsd2-pkg/index.js

---
+++ packages/bsd2-pkg/index.js
@@ -0,0 +1,4 @@
+// @license-testbed/bsd2-pkg
+// License: BSD-2-Clause
+// Demo package under BSD-2-Clause — permissive with no-endorsement
+module.exports = { name: '@license-testbed/bsd2-pkg', license: 'BSD-2-Clause' };
