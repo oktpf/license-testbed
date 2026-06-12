fab1468c62c87289934780b2adb187d839a68802
crawlingweapon <crawling.weapon700@ok9.io>
2026-06-12 13:58:15 -0500

    Add packages/lgpl-pkg/index.js

---
+++ packages/lgpl-pkg/index.js
@@ -0,0 +1,4 @@
+// @license-testbed/lgpl-pkg
+// License: LGPL-2.1-only
+// Demo package under LGPL-2.1 — weak copyleft, linking exception
+module.exports = { name: '@license-testbed/lgpl-pkg', license: 'LGPL-2.1-only' };
