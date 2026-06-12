9d663b5a9bbb9e34e7395f9bdf4a1af0e29c03d0
crawlingweapon <crawling.weapon700@ok9.io>
2026-06-12 13:58:24 -0500

    Add packages/zerobsd-pkg/index.js

---
+++ packages/zerobsd-pkg/index.js
@@ -0,0 +1,4 @@
+// @license-testbed/zerobsd-pkg
+// License: 0BSD
+// Demo package under 0BSD — zero-clause BSD, extremely permissive
+module.exports = { name: '@license-testbed/zerobsd-pkg', license: '0BSD' };
