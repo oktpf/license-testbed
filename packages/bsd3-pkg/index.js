cc14157eada300e4ac1f3ae10fd553992e20d127
crawlingweapon <crawling.weapon700@ok9.io>
2026-06-12 13:58:09 -0500

    Add packages/bsd3-pkg/index.js

---
+++ packages/bsd3-pkg/index.js
@@ -0,0 +1,4 @@
+// @license-testbed/bsd3-pkg
+// License: BSD-3-Clause
+// Demo package under BSD-3-Clause — permissive with non-endorsement clause
+module.exports = { name: '@license-testbed/bsd3-pkg', license: 'BSD-3-Clause' };
