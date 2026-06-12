33680c2c694ae7a285fd79b44098e998f0567d7a
crawlingweapon <crawling.weapon700@ok9.io>
2026-06-12 13:58:21 -0500

    Add packages/unlicense-pkg/index.js

---
+++ packages/unlicense-pkg/index.js
@@ -0,0 +1,4 @@
+// @license-testbed/unlicense-pkg
+// License: Unlicense
+// Demo package under Unlicense — public domain dedication
+module.exports = { name: '@license-testbed/unlicense-pkg', license: 'Unlicense' };
