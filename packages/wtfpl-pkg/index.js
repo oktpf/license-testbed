653533041c3cac0e3f6686b4ed2fbf2fcc21ea5a
crawlingweapon <crawling.weapon700@ok9.io>
2026-06-12 13:58:22 -0500

    Add packages/wtfpl-pkg/index.js

---
+++ packages/wtfpl-pkg/index.js
@@ -0,0 +1,4 @@
+// @license-testbed/wtfpl-pkg
+// License: WTFPL
+// Demo package under WTFPL — do-what-the-fuck-you-want public license
+module.exports = { name: '@license-testbed/wtfpl-pkg', license: 'WTFPL' };
